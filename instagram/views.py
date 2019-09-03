91828abc826aae15a9e0dbc33747497db8592970
magechaP <magechapeter@gmail.com>
2019-09-03 12:11:59 +0300

    create a user login and profile view objects

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from django.shortcuts import render,redirect
+from .models import Image,Profile,Like,Followers,Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import UpdateProfileForm,PostImage,CommentForm,UpdateImage
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    images = Image.objects.all().order_by('-post_date')
+    users = User.objects.all()  
+    current = request.user
+    likes = Like.objects.all().count()
+    
+    return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if current_user.id == user.id:
+        images = Image.objects.filter(owner_id=id)
+        current_user = request.user
+        user = User.objects.get(id=id)
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            return redirect(update_profile,current_user.id)
+    else: 
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            
+            return redirect(no_profile,id)      
+            
+    return render(request,'profile/profile.html',{'user':user,'profile':profile,'images':images,'current_user':current_user})
